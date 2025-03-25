06c4865c5eea8f786cc8580c624798b4fe9585d7
Navaraja <111477199+navaraja20@users.noreply.github.com>
2025-03-25 20:08:35 +0100

    Delete airflow directory

--- airflow/entrypoint.sh
+++
@@ -1,5 +0,0 @@
-# Inside ./airflow_spike/airflow/entrypoint.sh
-#!/bin/bash
-airflow db init
-airflow users create --username admin --password admin --firstname Admin --lastname User --role Admin --email admin@example.com
-airflow webserver & airflow scheduler
