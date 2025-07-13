9034a998e3a3e44668e4d08e6dc91acd03565b57
navaraja20 <navaraja2001@gmail.com>
2025-07-13 18:17:52 +0200

    Remove all files from main branch

--- airflow/alerting.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-def send_teams_alert(webhook_url, message):
-    headers = {"Content-Type": "application/json"}
-    data = {"text": message}
-    response = requests.post(webhook_url, json=data, headers=headers)
-    if response.status_code != 200:
-        raise ValueError(f"Request to Teams returned error {response.status_code}, {response.text}")
--- api/database/db.py
+++
@@ -1,9 +0,0 @@
-import os
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
-
-DATABASE_URL = os.getenv("DB_URL")  # Make sure this is set from .env
-
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
